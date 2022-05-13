a209f1eb72a5af337e6e35019c706fa3ebb26182
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-05-13 19:28:15 +0200

    Refactor twoToOneString exercise

--- twoToOneString.js
+++ twoToOneString.js
@@ -10,23 +10,35 @@ longest(a, b) -> "abcdefklmopqwxy"
 a = "abcdefghijklmnopqrstuvwxyz"
 longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"*/
 
+// refactor
+function longest(a,b){
+let cache = {};
+return [...a.split(''), ...b.split('')].filter(n=>{
+    // checks if the letter is already been assigned in the cache object
+    cache[n] = (cache[n]||0)+1;
 
-function longest(s1, s2) {
-    let cache = {}
-    let uniqueArray = s1.split('').filter(n=>{
+    // returns the letter that don't have a value higher than 1
+    return cache[n] <= 1
+}).sort().join('')
+};
+
+
+// function longest(s1, s2) {
+//     let cache = {}
+//     let uniqueArray = s1.split('').filter(n=>{
   
-      cache[n] = (cache[n]||0) + 1;
+//       cache[n] = (cache[n]||0) + 1;
   
-      return cache[n] <= 1
-    })
-    let uniqueArrayTwo = s2.split('').filter(n=>{
+//       return cache[n] <= 1
+//     })
+//     let uniqueArrayTwo = s2.split('').filter(n=>{
   
-      cache[n] = (cache[n]||0) + 1;
+//       cache[n] = (cache[n]||0) + 1;
   
-      return cache[n] <= 1
-    })
+//       return cache[n] <= 1
+//     })
     
-    let finallArray = [...uniqueArray, ...uniqueArrayTwo]
+//     let finallArray = [...uniqueArray, ...uniqueArrayTwo]
     
-    return finallArray.sort().join('')
-    }
+//     return finallArray.sort().join('')
+//     }
