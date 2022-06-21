6e22c53678f7107e60e35449439b84bcdf2cb0e6
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-06-21 19:27:39 +0200

    reverse the string

---
+++ reverseWords.js
@@ -0,0 +1,19 @@
+/* 
+Complete the function that accepts a string parameter,
+and reverses each word in the string.
+All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+
+*/
+
+function reverseWords(str) {
+
+    let tempArr = str.split(' ')
+    return tempArr.map(string=>{
+      return string.split('').reverse().join('')
+    }).join(' ')
+    
+    }
