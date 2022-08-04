494a867de84151fac76339dec37c3cf74a0fe5be
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-08-04 17:50:05 +0200

    Moving zeros to the end

---
+++ movingZerosToTheEnd.js
@@ -0,0 +1,20 @@
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, 
+preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+
+*/
+
+function moveZeros(arr) {
+    let countZeros = []
+    arr.forEach(num=>{
+      if(num === 0){
+        countZeros.push(num)
+      }
+    })
+  
+    let withoutZeros = arr.filter(num=>num !== 0) 
+    return [...withoutZeros, ...countZeros]
+    }
