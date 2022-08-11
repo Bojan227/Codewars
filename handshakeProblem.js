dd5d9674f8caef529b23df257b82009e68ac2ac2
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-08-11 19:24:16 +0200

    Handshake Problem

---
+++ handshakeProblem.js
@@ -0,0 +1,28 @@
+/*
+Johnny is a farmer and he annually holds a beet farmers convention "Drop the beet".
+
+Every year he takes photos of farmers handshaking. Johnny knows that no two farmers handshake more than once. He also knows that some of the possible handshake combinations may not happen.
+
+However, Johnny would like to know the minimal amount of people that participated this year just by counting all the handshakes.
+
+Help Johnny by writing a function, that takes the amount of handshakes and returns the minimal amount of people needed to perform these handshakes (a pair of farmers handshake only once).
+
+*/
+
+
+function getParticipants(handshakes) {
+    if (!handshakes) return 0;
+    let items = [1];
+  
+    for (let i = 0; i <= handshakes; i++) {
+      let n = items.length;
+      let combo = (n * (n - 1)) / 2;
+  
+      if (combo === handshakes || combo > handshakes) {
+        return items.length;
+      } else {
+        let arrItem = items[i];
+        items.push(arrItem + 1);
+      }
+    }
+  }
