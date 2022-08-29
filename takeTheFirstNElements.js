ca99ff27c8468f2b451e398851286fb42c46565d
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-08-29 17:46:05 +0200

    Usage of slice method

---
+++ takeTheFirstNElements.js
@@ -0,0 +1,13 @@
+/* 
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+If you need help, here's a reference:
+
+*/
+
+function take(arr, n) {
+    if (n === 0) return [];
+    if (arr.length === 0) return [];
+  
+    return arr.slice(0, n);
+  }
