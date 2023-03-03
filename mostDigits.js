df039df6bef6d3fac6407749abf36f083fb291c5
Bojan227 <blazevski.bojan1@gmail.com>
2023-03-03 10:48:44 +0100

    Find the number with the most digits

---
+++ mostDigits.js
@@ -0,0 +1,11 @@
+/*
+DESCRIPTION:
+Find the number with the most digits.
+
+If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+*/
+
+function findLongest(array){
+  return array.reduce((acc, curr)=>`${curr}`.length > `${acc}`.length ? curr : acc, 0 )
+}
